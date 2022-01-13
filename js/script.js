ceef9d097de65883e6e32d5776a91ab73e5c7dd4
floliq <41952434+floliq@users.noreply.github.com>
2022-01-13 21:36:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,14 +14,23 @@ const swiper = new Swiper(".swiper", {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
+  let swiperButtons = document.querySelectorAll(".swiper-pagination-bullet");
+  console.log(swiperButtons);
+  for (let i = 0; i < swiperButtons.length; i++) {
+    swiperButtons[i].setAttribute("aria-label", `Перейти на слайд ${i + 1}`);
+  }
   document.querySelectorAll(".job__button").forEach(function (jobButton) {
     jobButton.addEventListener("click", (event) => {
       let activeButton = document.querySelector(".job__button-checked");
       activeButton.classList.remove("job__button-checked");
+      activeButton.setAttribute("aria-current", "false");
       let path = event.currentTarget.dataset.info;
       document
         .querySelector(`[data-info="${path}"]`)
         .classList.add("job__button-checked");
+      document
+        .querySelector(`[data-info="${path}"]`)
+        .setAttribute("aria-current", "true");
       document.querySelectorAll(".job-content").forEach(function (jobContent) {
         jobContent.classList.remove("job-content-active");
       });
@@ -33,12 +42,24 @@ document.addEventListener("DOMContentLoaded", () => {
       ).style.background = `url("img/job/${path}.jpg")`;
     });
   });
+  if (window.innerWidth <= 1400) {
+    document.querySelector(".menu__bar").setAttribute("aria-hidden", "true");
+  }
 });
 
 var menuBtn = document.querySelector(".menu__btn");
 menuBtn.onclick = () => {
   menuBtn.classList.toggle("menu__btn-open");
   document.querySelector(".menu__bar").classList.toggle("menu__bar-open");
+  if (
+    document.querySelector(".menu__bar").classList.contains("menu__bar-open")
+  ) {
+    document.querySelector(".menu__bar").setAttribute("aria-hidden", "false");
+    menuBtn.setAttribute("aria-expanded", "true");
+  } else {
+    document.querySelector(".menu__bar").setAttribute("aria-hidden", "true");
+    menuBtn.setAttribute("aria-expanded", "false");
+  }
 };
 
 var menuSearch = document.querySelector(".menu-search");
@@ -46,10 +67,14 @@ menuSearch.addEventListener("click", () => {
   document.querySelector(".menu__form").classList.add("menu__form-active");
   menuSearch.style.pointerEvents = "none";
   menuSearch.style.opacity = "0";
+  document
+    .querySelector(".menu__form-active")
+    .setAttribute("aria-hidden", "false");
 });
 
 document.querySelector(".menu__close").addEventListener("click", () => {
   document.querySelector(".menu__form").classList.remove("menu__form-active");
   menuSearch.style.pointerEvents = "auto";
   menuSearch.style.opacity = "1";
+  document.querySelector(".menu__form").setAttribute("aria-hidden", "true");
 });
